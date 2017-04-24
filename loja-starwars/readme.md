bbdfcb7ec65aeb5d1bd300d871191bd1a548880c
Thiago Dias <thiagortz@gmail.com>
2017-04-24 19:18:33 -0300

    Adicionado configuração para heroku e database.

--- loja-starwars/readme.md
+++ loja-starwars/readme.md
@@ -1,5 +1,5 @@
 ##Tecnologias
-Java 8, Spring, Hibernate, Jackson, Maven, Apache Tomcat v8.5.
+Java 8, Spring, Hibernate, Jackson, Maven.
 
 
 ##br.com.stone.loja.confJPAConfiguration JPA Setup
@@ -9,5 +9,6 @@ Configuração do database adicionar login e senha
 
 Mysql database starstore.
 
-##Context root
-starstore
+
+##API REST URL
+https://starstore.herokuapp.com
--- loja-starwars/src/main/java/br/com/stone/loja/conf/JPAConfiguration.java
+++ loja-starwars/src/main/java/br/com/stone/loja/conf/JPAConfiguration.java
@@ -1,5 +1,7 @@
 package br.com.stone.loja.conf;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
@@ -19,53 +21,54 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-public class JPAConfiguration
-{
+public class JPAConfiguration {
 
-   @Bean
-   public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource)
-   {
-      LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-      em.setDataSource(dataSource);
-      em.setPackagesToScan(new String[] { "br.com.stone.loja.models" });
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource);
+		em.setPackagesToScan(new String[] { "br.com.stone.loja.models" });
 
-      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-      em.setJpaVendorAdapter(vendorAdapter);
-      em.setJpaProperties(additionalProperties());
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
 
-      return em;
-   }
+		return em;
+	}
 
-   @Bean
-   public DataSource dataSource(Environment environment)
-   {
-      DriverManagerDataSource dataSource = new DriverManagerDataSource();
-      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-      dataSource.setUrl("jdbc:mysql://localhost/starstore");
-      dataSource.setUsername("root");
-      dataSource.setPassword("123456");
-      return dataSource;
-   }
+	@Bean
+	public DataSource dataSource(Environment environment) throws URISyntaxException {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-   @Bean
-   public PlatformTransactionManager transactionManager(EntityManagerFactory emf)
-   {
-      JpaTransactionManager transactionManager = new JpaTransactionManager();
-      transactionManager.setEntityManagerFactory(emf);
-      return transactionManager;
-   }
+		URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
 
-   @Bean
-   public PersistenceExceptionTranslationPostProcessor exceptionTranslation()
-   {
-      return new PersistenceExceptionTranslationPostProcessor();
-   }
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl(dbUrl);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
 
-   Properties additionalProperties()
-   {
-      Properties properties = new Properties();
-      properties.setProperty("hibernate.hbm2ddl.auto", "update");
-      properties.setProperty("hibernate.show_sql", "false");
-      return properties;
-   }
+		return dataSource;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.show_sql", "false");
+		return properties;
+	}
 }
