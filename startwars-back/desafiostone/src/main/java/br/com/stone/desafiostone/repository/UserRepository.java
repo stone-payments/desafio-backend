993632b4e5b0ffbfe3ea140b44f97a0b0fa4a40c
DSntosxTech <juliano.santos88@etec.sp.gov.br>
2024-03-06 11:14:07 -0300

    firts commit

--- startwars-back/desafiostone/src/main/java/br/com/stone/desafiostone/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.stone.desafiostone.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.stone.desafiostone.models.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
--- startwars-back/desafiostone/src/main/java/br/com/stone/desafiostone/service/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package br.com.stone.desafiostone.service;
-
-public interface SecurityService {
-    String findLoggedInUsername();
-
-    void autologin(String username, String password);
-}
